f60fcc537882c0117ff12d373e4a91f45a85fc2f
HenadziLitvinenka <henadzi.litvinenka@productmadness.com>
2021-10-03 13:54:15 +0300

    Include test player

---
+++ Assets/Scripts/SOLID/CameraFollower.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class CameraFollower : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+
+        [SerializeField] private float smoothTime = 0.3f;
+
+        [SerializeField] private Vector3 Offset;
+
+        private Vector3 velocity = Vector3.zero;
+
+        private void Start()
+        {
+            Offset = transform.position - _target.position;
+        }
+
+        private void LateUpdate()
+        {
+            FollowTarget(_target);
+        }
+
+        private void FollowTarget(Transform target)
+        {
+            Vector3 targetPosition = target.position + Offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
---
+++ Assets/Scripts/SOLID/GetPlayerInput.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+    public class GetPlayerInput : MonoBehaviour
+    {
+        public float Horizontal { get; private set; }
+        public float Vertical { get; private set; }
+        public bool FireWeapon { get; private set; }
+
+        public event Action OnFire = delegate { };
+
+        private PhotonView view;
+
+        private void Start()
+        {
+            view = GetComponent<PhotonView>();
+
+            if (!view.IsMine)
+            {
+                
+            }
+        }
+
+        private void Update()
+        {
+            if (!view.IsMine) return;
+            
+            Horizontal = Input.GetAxis("Horizontal");
+            Vertical = Input.GetAxis("Vertical");
+            FireWeapon = Input.GetKey(KeyCode.Mouse0);
+
+            if (FireWeapon)
+            {
+                OnFire();
+            }
+        }
+    }
